48caa93504bdce4ef73449f0f8addf23e911954e
Fran Salanova <fsalanova.16@gmail.com>
2024-03-11 23:52:22 -0300

    Working on positioning the tooltip

--- tooltip.js
+++ tooltip.js
@@ -7,6 +7,7 @@ document.body.append(tooltipContainer)
 addGlobalEventListener("mouseover", "[data-tooltip]", (e) => {
   const tooltip = createToolTipElement(e.target.dataset.tooltip)
   tooltipContainer.append(tooltip)
+  positionTooltip(tooltip, e.target)
 
   e.target.addEventListener(
     "mouseleave",
@@ -22,4 +23,28 @@ function createToolTipElement(text) {
   tooltip.classList.add("tooltip")
   tooltip.textContent = text
   return tooltip
+}
+
+function positionTooltip(tooltip, element) {
+  const elementRect = element.getBoundingClientRect()
+  const tooltipRect = tooltip.getBoundingClientRect()
+
+  tooltip.style.top = `${elementRect.top - tooltipRect.height}px`
+  tooltip.style.left = `${
+    elementRect.left + elementRect.width / 2 - tooltipRect.width / 2
+  }px`
+
+  const bounds = isOutOfBounds(tooltip)
+}
+
+function isOutOfBounds(element) {
+  const rect = element.getBoundingClientRect()
+  const container = tooltipContainer.getBoundingClientRect()
+
+  return {
+    left: rect.left < container.left,
+    right: rect.right > container.right,
+    top: rect.top < container.top,
+    bottom: rect.bottom > container.bottom,
+  }
 }
