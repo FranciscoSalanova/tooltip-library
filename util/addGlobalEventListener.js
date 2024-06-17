f293ff47a3e0a80c9331216cd7f126ef590a854a
Fran Salanova <fsalanova.16@gmail.com>
2024-06-17 18:12:20 -0300

    Adding docs

--- util/addGlobalEventListener.js
+++ util/addGlobalEventListener.js
@@ -1,3 +1,9 @@
+/** Creates a new global event listener, this is an event listener at the document level.
+ *
+ * @param {string} type - The type of DOM event to be listened to.
+ * @param {string} selector - The DOM selector of the element that will trigger the callback.
+ * @param {function} callback - The callback function that will executed in response of the event.
+ */
 export default function addGlobalEventListener(type, selector, callback) {
   document.addEventListener(type, (e) => {
     if (e.target.matches(selector)) {
