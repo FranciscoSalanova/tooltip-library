7bcc91eb37be9d26982f83bcc377349c9c9af8c5
Francisco Salanova <fs185092@ncr.com>
2024-06-18 19:36:30 -0300

    Adding left and right controls for keeping the tooltip rendered inside the viewport

--- tooltip.js
+++ tooltip.js
@@ -1,5 +1,7 @@
 import addGlobalEventListener from "./util/addGlobalEventListener.js"
 
+const DEFAULT_SPACING = 5
+
 const tooltipContainer = document.createElement("div")
 tooltipContainer.classList.add("tooltip-container")
 document.body.append(tooltipContainer)
@@ -22,29 +24,50 @@ function createToolTipElement(text) {
   const tooltip = document.createElement("div")
   tooltip.classList.add("tooltip")
   tooltip.textContent = text
+
   return tooltip
 }
 
 function positionTooltip(tooltip, element) {
   const elementRect = element.getBoundingClientRect()
   const tooltipRect = tooltip.getBoundingClientRect()
+  const spacing = parseInt(element.dataset.spacing) || DEFAULT_SPACING
 
-  tooltip.style.top = `${elementRect.top - tooltipRect.height}px`
+  tooltip.style.top = `${elementRect.top - tooltipRect.height - spacing}px`
   tooltip.style.left = `${
     elementRect.left + elementRect.width / 2 - tooltipRect.width / 2
   }px`
 
-  const bounds = isOutOfBounds(tooltip)
+  const bounds = isOutOfBounds(tooltip, spacing)
+
+  if (bounds.top) {
+    resetTooltipPosition(tooltip)
+  }
+
+  if (bounds.right) {
+    tooltip.style.right = `${spacing}px`
+    tooltip.style.left = "initial"
+  }
+  if (bounds.left) {
+    tooltip.style.left = `${spacing}px`
+  }
 }
 
-function isOutOfBounds(element) {
+function isOutOfBounds(element, spacing) {
   const rect = element.getBoundingClientRect()
   const container = tooltipContainer.getBoundingClientRect()
 
   return {
-    left: rect.left < container.left,
-    right: rect.right > container.right,
-    top: rect.top < container.top,
-    bottom: rect.bottom > container.bottom,
+    left: rect.left <= container.left + spacing,
+    right: rect.right >= container.right - spacing,
+    top: rect.top <= container.top + spacing,
+    bottom: rect.bottom >= container.bottom - spacing,
   }
+}
+
+function resetTooltipPosition(element) {
+  element.style.left = "initial"
+  element.style.right = "initial"
+  element.style.top = "initial"
+  element.style.bottom = "initial"
 }
